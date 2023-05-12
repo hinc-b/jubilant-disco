5b2493b0a42174b25701e938dd293d32856c6473
Blazej Hinc <blazejh@google.com>
2023-05-12 16:59:38 +0200

    Firestore and Storage implementation

---
+++ app/src/main/java/com/example/certuploaderimage/ImagesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.certuploaderimage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/certuploaderimage/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.certuploaderimage
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.certuploaderimage.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var storageRef: StorageReference
+    private lateinit var firebaseFirestore: FirebaseFirestore
+    private var imageUri : Uri? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+
+        initVars()
+        registerClickEvents()
+    }
+
+    private fun registerClickEvents(){
+        binding.uploadBtn.setOnClickListener {
+
+        }
+
+        binding.showAllBtn.setOnClickListener {
+            startActivity(Intent(this, ImagesActivity::class.java))
+        }
+
+        binding.imageView.setOnClickListener {
+            resultLauncher.launch("image/*")
+        }
+    }
+
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.GetContent()) {
+
+        imageUri = it
+        binding.imageView.setImageURI(it)
+    }
+
+    private fun initVars() {
+        storageRef = FirebaseStorage.getInstance().reference.child("Images")
+        firebaseFirestore = FirebaseFirestore.getInstance()
+    }
+}
