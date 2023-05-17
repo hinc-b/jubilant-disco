6eb65bf7c5ec8fcb9ea639918e6ef3562c93de58
Blazej Hinc <blazejh@google.com>
2023-05-17 15:18:14 +0200

    Upload acitivity finished

---
+++ app/src/main/java/com/example/myapplication/ImagesActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.databinding.ActivityImagesBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
+
+class ImagesActivity : AppCompatActivity() {
+
+}
